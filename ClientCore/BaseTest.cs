dc4dd05b698e3b6525d6f13893ad0befa6748343
xiaomo <xiaomo@xiaomo.info>
2017-08-26 10:13:38 +0800

    add function :fire:

---
+++ ClientCore/BaseTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClientCore
+{
+    public class BaseTest
+    {
+
+        public void Test()
+        {
+            Console.WriteLine("测试类库主法.........");
+        }
+
+    }
+}
--- ClientCore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClientCore
-{
-    public class Class1
-    {
-    }
-}
